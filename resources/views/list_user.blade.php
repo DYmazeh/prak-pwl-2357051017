b2259b2db347e86913eb4ced254d4c57b0afac0a
DYmazeh <dymaz.satya2005@gmail.com>
2025-10-05 00:41:55 +0700

    Tugas Pertemuan 4
    Menambahkan desain modern, komponen navbar, footer, dan tabel dinamis.

--- resources/views/list_user.blade.php
+++ resources/views/list_user.blade.php
@@ -1,3 +1,4 @@
+
 @extends('layouts.app')
 @section('content')
 <div class="card shadow-sm">
@@ -6,32 +7,7 @@
             <h3 class="mb-0">Daftar Pengguna</h3>
             <a href="{{ route('user.create') }}" class="btn btn-primary">+ Tambah User</a>
         </div>
-        <div class="table-responsive">
-            <table class="table table-hover align-middle">
-                <thead>
-                    <tr>
-                        <th>ID</th>
-                        <th>Nama</th>
-                        <th>NPM</th>
-                        <th>Kelas</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    @forelse ($users as $user)
-                    <tr>
-                        <td>{{ $user->id }}</td>
-                        <td>{{ $user->nama }}</td>
-                        <td>{{ $user->npm }}</td>
-                        <td>{{ $user->nama_kelas }}</td>
-                    </tr>
-                    @empty
-                    <tr>
-                        <td colspan="4" class="text-center text-muted">Belum ada data pengguna.</td>
-                    </tr>
-                    @endforelse
-                </tbody>
-            </table>
-        </div>
+        @include('components.user-table', ['users' => $users])
     </div>
 </div>
 @endsection
